bfbdc2c17cb847dc5e6d1fe404d54a35070702bc
emohamed <mrbd381@gmail.com>
2023-03-21 14:24:32 -0700

    fix code

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,6 +7,7 @@
  */
 
 int print_last_digit(int n)
+
 {
 
 	int last;
@@ -14,8 +15,8 @@ int print_last_digit(int n)
 	last = n % 10;
 	if (last < 0)
 	{
-		last = last * -1;
+	last = last * -1;
 	}
 	_putchar(last + '0');
 	return (last);
-} 
+}
